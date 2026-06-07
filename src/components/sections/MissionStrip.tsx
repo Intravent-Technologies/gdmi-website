763e799ab7a5ef0fc466c0d7c619c22e0486ec8c
these <these@users.noreply.github.com>
2026-06-07 16:09:46 +0100

    Fix MissionStrip images: update to proper URLs with onError fallback

--- src/components/sections/MissionStrip.tsx
+++ src/components/sections/MissionStrip.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 const missions = [
   {
     image:
-      "https://scontent-lhr6-2.xx.fbcdn.net/v/t39.30808-6/484871540_626450703534653_3582917198373400068_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEKslQblNSlgHychrX_sKlpoVsj1RjXAsuhWyPVGNcCyytMwmIlJC3hDixeuGeEqtq-8vnpCyT6uzPE-cABe8ZO&_nc_ohc=nct1aokkq2oQ7kNvwGDrhTK&_nc_oc=AdpcgEHNe9wWzsSt9QdGy5q8u6hoDWM1gfgudqJNDMxsgkTKHbdTU35p3AGY4guNy8o&_nc_zt=23&_nc_ht=scontent-lhr6-2.xx&_nc_gid=zdmh6Gn9Jkp8Bf79dBvCDQ&_nc_ss=7b2a8&oh=00_Af-c6oz715TfQ6UqFmKlC4PuApN_jaLgikHdwygTBxdTiw&oe=6A242914",
+      "https://scontent-lhr8-2.xx.fbcdn.net/v/t39.30808-6/495217322_668921812620875_8688358263259804968_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x768&ctp=s1080x768&_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEUc14OjA3Bt06LqbLAjZQKbPm6p-DpPbFs-bqn4Ok9sarn9as_VdPwT63R9fOVsrlua9IughpTgGWrOpy7U7nz&_nc_ohc=pp8eRHwca6wQ7kNvwED3MuY&_nc_oc=AdrYxFX0oaMb8te0y8QEj_9BdNbRDRohL4EoH8F4_cl2X4LzOYTshY2Qo_1ogW8BEIg&_nc_zt=23&_nc_ht=scontent-lhr8-2.xx&_nc_gid=5cM8tMLsaBpsY6_3cvtvaA&_nc_ss=7b2a8&oh=00_Af8mvOc-vq-MpCeSMbiCQ9pjuwu4-fxIGMnSAEQcTs29JA&oe=6A2B70F1",
     symbol: "01",
     title: "Prophecy",
     description:
@@ -9,7 +11,7 @@ const missions = [
   },
   {
     image:
-      "https://scontent-lhr6-2.xx.fbcdn.net/v/t39.30808-6/484895890_625466766966380_217842608764944892_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF63BNSYxa7gUTI-JpK03LC1-BIOR6foB_X4Eg5Hp-gHwnYftug6BY9HdtQUpAf-VPSruOnw-n_QeLeVpvTZEQQ&_nc_ohc=StYlhznTPfUQ7kNvwHB7AYC&_nc_oc=Adq0W0IRGMso8PECZKPKcCUo9ctSdHiMDINQpuh1LfTg8aA0bsDmNtXG6GRmTrQF3hE&_nc_zt=23&_nc_ht=scontent-lhr6-2.xx&_nc_gid=Ky7hwaz_OEyj9GmtG5u9EQ&_nc_ss=7b2a8&oh=00_Af_YTSfHE0O885X4SdmD-rGEjQNncQTe7gSz1PEM-QPcLw&oe=6A243180",
+      "https://scontent-lhr6-2.xx.fbcdn.net/v/t39.30808-6/484895890_625466766966380_217842608764944892_n.jpg?stp=dst-jpg_tt6&cstp=mx843x843&ctp=s843x843&_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF63BNSYxa7gUTI-JpK03LC1-BIOR6foB_X4Eg5Hp-gHwnYftug6BY9HdtQUpAf-VPSruOnw-n_QeLeVpvTZEQQ&_nc_ohc=brGtynaGU9AQ7kNvwF5xil7&_nc_oc=AdqZpAXeGKlwDk4uu9z-3H9rDA2mXu_4KQRBew8KOJ3F7MyXLzhCzd67pTKKuCeeXNU&_nc_zt=23&_nc_ht=scontent-lhr6-2.xx&_nc_gid=c1JfuuAnKlJhFM0uZg_Qtw&_nc_ss=7b2a8&oh=00_Af-RZlZ9KEgTTeSzjOwIflj3jQ0-ccq97cJFE_HLdOpFHQ&oe=6A2B71C0",
     symbol: "02",
     title: "Evangelism",
     description:
@@ -17,7 +19,7 @@ const missions = [
   },
   {
     image:
-      "https://scontent-lhr6-2.xx.fbcdn.net/v/t39.30808-6/482247380_625467900299600_919649910819387116_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGd6tBmX_QsRkMsMAqYAJLLIkXu4LM4SmciRe7gszhKZwOmpnWfpAiRQpC-ygSqeA35gjrlZzGuuvKJHeyczmkZ&_nc_ohc=-b75xOhDMtIQ7kNvwGFnZyX&_nc_oc=AdqBwr3Xs7PXFma8ZMIRC17IFQ0FLveYYVH3OFCKoKiURHzU3DTVJYWm1dnAeF_uDQg&_nc_zt=23&_nc_ht=scontent-lhr6-2.xx&_nc_gid=scsQKgFdmahZwSgfem3pGw&_nc_ss=7b2a8&oh=00_Af_B-ieoAE3-04LB_EV3Jjoyw-AnmUnVuZ5v1p2P5Odd8Q&oe=6A2449F6",
+      "https://scontent-lhr6-2.xx.fbcdn.net/v/t39.30808-6/482246794_625467790299611_6473732889479460716_n.jpg?stp=dst-jpg_tt6&cstp=mx1440x1440&ctp=s1440x1440&_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHItikPFalHgWre_QgRSxGNZblFUfQvEqtluUVR9C8Sq6jISXdSWtmNbgAJnvnyP4D3DfjMwk6fyXN_P1g1Mrlr&_nc_ohc=rNw7yBcdZz4Q7kNvwEBtkys&_nc_oc=AdrDHdn7DyNZ7HhcZDdKmrCzsBeeTONZPEUWT0zxZjCHWztxsjd5fZ42nbBeQtk1efM&_nc_zt=23&_nc_ht=scontent-lhr6-2.xx&_nc_gid=-5CMefiFU8o0e1tV3R4D1g&_nc_ss=7b2a8&oh=00_Af_Bi7Y-eUCVcFOr6uYrKXy1F9jXj8YO24gNUV49dinBNA&oe=6A2B76EC",
     symbol: "03",
     title: "Discipleship",
     description:
@@ -38,6 +40,10 @@ export function MissionStrip() {
               <img
                 src={m.image}
                 alt={m.title}
+                loading="lazy"
+                onError={(e) => {
+                  e.currentTarget.src = "/images/placeholder-project.svg";
+                }}
                 className="absolute inset-0 w-full h-full object-cover group-hover:scale-105 transition-transform duration-700"
               />
               <div className="absolute inset-0 bg-gradient-to-t from-primary via-primary/60 to-primary/30" />
